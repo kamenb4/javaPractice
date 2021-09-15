ce079b923764ea5efbe76728e53c69ade14f7d6d
mac <kamenb04@gmail.com>
2021-09-15 20:22:03 +0300

    added POJO Country

---
+++ 04_Encapsulation/02_Pojo/src/Country.java
@@ -0,0 +1,51 @@
+public class Country {
+    public String name;
+    public int count;
+    public int square;
+    public String capital;
+    public String sea;
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getSquare() {
+        return square;
+    }
+
+    public void setSquare(int square) {
+        this.square = square;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getSea() {
+        return sea;
+    }
+
+    public void setSea(String sea) {
+        this.sea = sea;
+    }
+}
