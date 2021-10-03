6c0118e6def9b714c7baaf9925134a01ad23fe52
mac <kamenb04@gmail.com>
2021-10-03 18:30:37 +0300

    fixed Container and add russian alphabet

--- 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
+++ 06_PrimitiveDataTypes/IntOrInteger/src/Main.java
@@ -4,6 +4,14 @@ public class Main {
         container.addCount(5672);
         System.out.println(container.getCount());
 
+
+        for(char alph = 'Ё'; alph <= 'ё'; alph++) {
+            if (alph > 'Ё' && alph < 'А' || alph > 'я' && alph < 'ё'){
+                continue;
+            }
+            System.out.println(alph + " (" + ((int) alph) + ")");
+        }
+
         // TODO: ниже напишите код для выполнения задания:
         //  С помощью цикла и преобразования чисел в символы найдите все коды
         //  букв русского алфавита — заглавных и строчных, в том числе буквы Ё.
