2add45b066c4770e87ac314724f45073bd9e97e4
mac <kamenb04@gmail.com>
2021-10-25 00:00:31 +0300

    TotalEarnings

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,6 +3,14 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    String first = "";
+    for (int i = 0; i < text.length(); i++) {
+      char charStr = text.charAt(i);
+      if(Character.isDigit(charStr)) {
+        first += charStr;
+      }
+    }
+    System.out.println(first);
     //TODO: напишите ваш код, результат вывести в консоль
   }
 
