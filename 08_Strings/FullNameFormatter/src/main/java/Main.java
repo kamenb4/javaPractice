5dc8ce23629b53c5e70feb979840368fceeb8c5a
mac <kamenb04@gmail.com>
2021-11-02 18:30:08 +0300

    First part of homework

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -5,29 +5,46 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    String encoding1 = "";
-    String encoding2 = "";
-    String encoding3 = "";
-    while (true) {
-      String input = scanner.nextLine();
+    String encoding1, encoding2, encoding3;
+    String input = scanner.nextLine();
+
       int flag1 = input.indexOf(" ");
       int flag2 = input.indexOf(" ", flag1 + 1);
-        if (flag1 != -1 && flag2 != -1) {
-          encoding1 = input.substring(0, flag1);
-          encoding2 = input.substring(flag1, flag2);
-          encoding3 = input.substring(flag2);
 
+      int spaceCount = 0;
+      for(char ch : input.toCharArray()) {
+        if(ch == ' ') {
+          spaceCount++;
+        }
       }
-      if (input.equals("0")) {
-        break;
+
+    for(int i = 0; i < input.length(); i++) {
+
+      char c = input.charAt(i);
+
+      if(Character.isDigit(c)) {
+        System.out.println("Введенная строка не является ФИО");
+        return;
+
+      } else if ((spaceCount == 2) && (flag1 != -1 && flag2 != -1)) {
+        encoding1 = input.substring(0, flag1);
+        encoding2 = input.substring(flag1, flag2);
+        encoding3 = input.substring(flag2);
+        System.out.println("Фамилия: " + encoding1);
+        System.out.println("Имя:" + encoding2);
+        System.out.println("Отчество:" + encoding3);
+        return;
       }
+      if(spaceCount != 2) {
+        System.out.println("Введенная строка не является ФИО");
+        return;
+      }
+    }
+
 
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-    }
-    System.out.println("Фамилия: " + encoding1);
-    System.out.println("Имя:" + encoding2);
-    System.out.println("Отчество:" + encoding3);
+
   }
 
 }
--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,14 +3,18 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    String first = "";
-    for (int i = 0; i < text.length(); i++) {
-      char charStr = text.charAt(i);
-      if(Character.isDigit(charStr)) {
-        first += charStr;
-      }
+    String charsetStr = "руб";
+    int start = 0;
+    int sum = 0;
+
+    while (start != -1) {
+      int end = text.indexOf(charsetStr, start);
+      start = text.lastIndexOf(" ", end - 2);
+      sum += Integer.parseInt(text.substring(start, end).trim());
+      start = text.indexOf(" ", end);
     }
-    System.out.println(first);
+
+    System.out.println(sum);
     //TODO: напишите ваш код, результат вывести в консоль
   }
 
