be3a25741df168e60690b0559d78367a1912ef3b
mac <kamenb04@gmail.com>
2021-12-18 11:22:48 +0300

    added reverseArray and Hospital start

--- 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
+++ 09_ArraysAndCollections/homework_9.1/Hospital/src/main/java/Hospital.java
@@ -1,22 +1,37 @@
+import java.util.Arrays;
 public class Hospital {
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
-
-        return new float[0];
+        float[] patientsTemperature = new float[patientsCount];
+        for(int i = 0; i < patientsTemperature.length; i++) {
+            patientsTemperature[i] = (float) (Math.round(((float) (Math.random() * 8) + 32) * 10.0) / 10.0);
+        }
+        return patientsTemperature;
     }
 
     public static String getReport(float[] temperatureData) {
+        temperatureData = generatePatientsTemperatures(30);
+
+        String temp = Arrays.toString(temperatureData);
+        temp += temp.trim();
         /*
         TODO: Напишите код, который выводит среднюю температуру по больнице,количество здоровых пациентов,
             а также температуры всех пациентов.
         */
+        int count = 0;
+        for (int i = 0; i < temperatureData.length; i++) {
+            if (temperatureData[i] > 32.0 && temperatureData[i] < 40.0) {
+                count++;
+            }
+        }
+
 
         String report =
-                "Температуры пациентов: " + 0 +
+                "Температуры пациентов: " + temp +
                         "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+                        "\nКоличество здоровых: " + count;
 
         return report;
     }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,12 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+
+        for (int i = 0; i < strings.length / 2; i++) {
+            String str = strings[i];
+            strings[i] = strings[strings.length - i - 1];
+            strings[strings.length - i - 1] = str;
+        }
         return strings;
     }
 }
