4ee513fdb9df8ab79531f7bae954e22b8b4bc3f1
mac <kamenb04@gmail.com>
2022-05-02 16:03:13 +0300

    fixed company1

--- InheritanceAndPolymorphism/homework_4/src/CompanyMain.java
+++ InheritanceAndPolymorphism/homework_4/src/CompanyMain.java
@@ -2,15 +2,19 @@
 public class CompanyMain {
     public static void main(String[] args) {
         Company company = new Company();
+        TopManager topManager = new TopManager();
+        Operator operator = new Operator(30_000);
+        Manager manager = new Manager();
         company.setIncome(56_456_753.0);
 
         company.hire(new TopManager());
         company.hireAll(new TopManager(140_000.0), 50);
         company.hireAll(new Manager(), 130);
-        company.hireAll(new Operator(100_000), 30);
+        company.hireAll(operator, 30);
         company.fire(new TopManager());
         System.out.println(company.staff.size());
         company.fireHalf();
+        company.fire(operator);
         System.out.println(company.staff.size());
 
 
